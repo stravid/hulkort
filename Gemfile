d560453322f87ce24a50b2910867acca8a2e1dce
David Strauß <mail@stravid.com>
2011-09-18 16:51:43 +0200

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'jquery-rails'
+gem 'pg'
+gem 'clearance'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+group :test do
+  gem 'turn', :require => false
+  gem 'capybara'
+  gem 'cucumber-rails'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'shoulda'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hulkort::Application.config.secret_token = '2e2abe7697c03d7d7813817d1ccd7681e79249fa1dbba24cdee420598c0f279f9ac5943226c0089b0fb1927e6a4194a94750e5c78e4c329c3a2cbe69f93cc141'
