3f125c477b3d3d3fb051808ef9667be4803a833e
Thomas Buchöster <thomas.buchoester@gmail.com>
2011-10-10 02:03:48 +0200

    now using 'spork' and 'guard' for faster and more convenient testing

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'jquery-rails'
 gem 'pg'
 gem 'clearance'
 gem 'high_voltage'
+gem 'rack', '1.3.3' # limit version to 1.3.3 because of warning in v 1.3.4
 
 group :assets do
   gem 'sass-rails', "  ~> 3.1.0"
@@ -17,8 +18,13 @@ group :test do
   gem 'capybara'
   gem 'launchy'
   gem 'cucumber-rails', "~>1.1.0"
-  gem 'database_cleaner'
   gem 'factory_girl_rails'
+  gem 'rb-fsevent', :require => false if RUBY_PLATFORM =~ /darwin/i # Mac OSX 10
+  gem 'spork', '> 0.9.0.rc'
+  gem 'guard-spork'
+  gem 'guard-rspec'
+  gem 'guard-cucumber'
+  gem 'database_cleaner'
 end
 
 group :test, :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,44 +1,41 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  # This file is copied to spec/ when you run 'rails generate rspec:install'
 
-RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  RSpec.configure do |config|
+    # == Mock Framework
+    # Mock Framework options are: mocha, flexmock or RR
+    config.mock_with :rspec
 
-  # Database Cleaner settings
-  # Changed before and after filters from :each to :all to allow creating
-  # persistent records in specs using before(:all). It should be enough to clean
-  # database once after all specs finished
-  config.before(:suite) do
+    config.use_transactional_fixtures = true
+
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true
+
+    # For original DatabaseCleaner setting watch github repo at https://github.com/bmabey/database_cleaner
     DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.logger = Rails.logger
   end
 
-  config.before(:all) do
-    DatabaseCleaner.start
-  end
+end
 
-  config.after(:all) do
-    DatabaseCleaner.clean
-  end
+Spork.each_run do
+
+  FactoryGirl.reload
+  DatabaseCleaner.clean
 
 end
