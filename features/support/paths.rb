396653b90e3e77decf64af21ba969ed0d30e21ef
David Strauß <mail@stravid.com>
2011-11-15 23:21:15 +0100

    Use graph_path instead of url_for

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -21,7 +21,7 @@ module NavigationHelpers
       edit_user_path(current_user)
     when /the "(.*)" repository graph page/i
       repository = Repository.find_by_name($1)
-      url_for(:controller => 'graphs', :action => 'show', :shortcode => repository.shortcode)
+      graph_path(:shortcode => repository.shortcode)
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
